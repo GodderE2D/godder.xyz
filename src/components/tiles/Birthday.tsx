dfec4e3955c7e0ecb4ef97f9673e838861f8a663
GodderE2D <55918888+GodderE2D@users.noreply.github.com>
2023-08-21 20:09:24 +0800

    fix: birthday being cached; actual placeholders

--- src/components/tiles/Birthday.tsx
+++ src/components/tiles/Birthday.tsx
@@ -1,24 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { DateTime } from "luxon";
 
+// You have to change the human-friendly date on line 37 too
 const MONTH = 5 as const;
 const DAY = 7 as const;
 
 export default function Birthday() {
-  const now = DateTime.now().startOf("day");
+  const [days, setDays] = useState<number>();
 
-  const birthdayPassed = now.month >= MONTH && now.day >= DAY;
-  const birthday = DateTime.fromObject({
-    year: birthdayPassed ? now.year + 1 : now.year,
-    month: MONTH,
-    day: DAY,
-  });
+  useEffect(() => {
+    const now = DateTime.now().startOf("day");
 
-  const days = birthday.diff(now, "days").days;
+    const birthdayPassed = now.month >= MONTH && now.day >= DAY;
+    const birthday = DateTime.fromObject({
+      year: birthdayPassed ? now.year + 1 : now.year,
+      month: MONTH,
+      day: DAY,
+    });
+
+    setDays(birthday.diff(now, "days").days);
+  }, []);
 
   return (
     <div className="group col-span-2 row-span-1 rounded-2xl bg-lime-950 p-4 text-center shadow md:col-span-1">
       <h2 className="mb-0.5 bg-gradient-to-tl from-lime-500 to-lime-300 bg-clip-text text-xl font-medium text-transparent md:mb-2">
-        {days === 366 ? "Today" : `${days} day${days !== 1 ? "s" : ""}`}
+        {days ? (days === 366 ? "Today" : `${days} day${days !== 1 ? "s" : ""}`) : "--"}
       </h2>
       {days === 366 ? (
         <p className="text-xs">is my birthday! 🎉</p>
@@ -26,7 +34,7 @@ export default function Birthday() {
         <>
           <p className="text-xs group-hover:hidden">until my birthday</p>
           <p className="hidden text-xs group-hover:block">
-            until <strong>{birthday.toFormat("d MMMM")}</strong>
+            until <strong>7 May</strong>
           </p>
         </>
       )}
--- src/components/tiles/Time.tsx
+++ src/components/tiles/Time.tsx
@@ -7,8 +7,11 @@ const TIMEZONE = "Asia/Shanghai" as const;
 
 export default function Time() {
   const timezone = DateTime.now().setZone(TIMEZONE).toFormat("Z");
-  const firstTime = DateTime.now().setZone(TIMEZONE).toFormat("T");
-  const [time, setTime] = useState(firstTime);
+  const [time, setTime] = useState("");
+
+  useEffect(() => {
+    setTime(DateTime.now().setZone(TIMEZONE).toFormat("T"));
+  }, []);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -16,12 +19,12 @@ export default function Time() {
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [firstTime]);
+  }, [time]);
 
   return (
     <div className="col-span-1 row-span-1 rounded-2xl bg-purple-950 p-4 text-center shadow">
-      <h2 className="mb-0.5 bg-gradient-to-tl from-purple-500 to-purple-300 bg-clip-text text-xl font-medium text-transparent md:mb-2">
-        {time}
+      <h2 className="mb-0.5 h-7 bg-gradient-to-tl from-purple-500 to-purple-300 bg-clip-text text-xl font-medium text-transparent md:mb-2">
+        {time || "--"}
       </h2>
       <p className="text-xs">
         in <strong>UTC{timezone}</strong>
